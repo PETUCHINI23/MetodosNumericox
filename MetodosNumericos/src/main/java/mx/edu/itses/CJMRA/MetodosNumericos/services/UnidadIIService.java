20f0a88a0e7833c0550f52d14427dfb842206b56
PETUCHINI23 <l.raoc050223@itses.edu.mx>
2025-06-19 09:49:02 -0600

    Este si es el de regla falsa :)

---
+++ MetodosNumericos/src/main/java/mx/edu/itses/CJMRA/MetodosNumericos/services/UnidadIIService.java
@@ -0,0 +1,13 @@
+package mx.edu.itses.CJMRA.MetodosNumericos.services;
+
+import java.util.ArrayList;
+import mx.edu.itses.CJMRA.MetodosNumericos.domain.Biseccion;
+import mx.edu.itses.CJMRA.MetodosNumericos.domain.ReglaFalsa;
+
+public interface UnidadIIService {
+     public ArrayList<Biseccion> AlgoritmoBiseccion(Biseccion biseccion);
+     
+     public ArrayList<ReglaFalsa> AlgoritmoReglaFalsa(ReglaFalsa reglafalsa);
+
+     
+}
---
+++ MetodosNumericos/src/main/java/mx/edu/itses/CJMRA/MetodosNumericos/services/UnidadIIServiceImpl.java
@@ -0,0 +1,121 @@
+package mx.edu.itses.CJMRA.MetodosNumericos.services;
+
+import java.util.ArrayList;
+import lombok.extern.slf4j.Slf4j;
+import mx.edu.itses.CJMRA.MetodosNumericos.domain.Biseccion;
+
+import mx.edu.itses.CJMRA.MetodosNumericos.domain.ReglaFalsa;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UnidadIIServiceImpl implements UnidadIIService {
+
+    @Override
+    public ArrayList<Biseccion> AlgoritmoBiseccion(Biseccion biseccion){
+        ArrayList<Biseccion> respuesta = new ArrayList<>();
+        double XL, XU, XRa, XRn, FXL, FXU, FXR, Ea;
+
+        XL = biseccion.getXl();
+        XU = biseccion.getXu();
+        XRa = 0;
+        Ea = 100;
+        // Verificamos que en el intervalo definido haya un cambio de signo
+        FXL = Funciones.Ecuacion(biseccion.getFx(), XL);
+        FXU = Funciones.Ecuacion(biseccion.getFx(), XU);
+        if (FXL * FXU < 0) {
+            for (int i = 1; i <= biseccion.getIteracionesMaximas(); i++) {
+                XRn = (XL + XU) / 2;
+                FXL = Funciones.Ecuacion(biseccion.getFx(), XL);
+                FXU = Funciones.Ecuacion(biseccion.getFx(), XU);
+                FXR = Funciones.Ecuacion(biseccion.getFx(), XRn);
+                if (i != 1) {
+                    Ea = Funciones.ErrorRelativo(XRn, XRa);
+                }
+                Biseccion renglon = new Biseccion();
+                renglon.setXl(XL);
+                renglon.setXl(XU);
+                renglon.setXr(XRn);
+                renglon.setFXl(FXL);
+                renglon.setFXu(FXU);
+                renglon.setFXr(FXR);
+                renglon.setEa(Ea);
+                if (FXL * FXR < 0) {
+                    XU = XRn;
+                } else if (FXL * FXR > 0) {
+                    XL = XRn;
+                } else if (FXL * FXR == 0) {
+                    break;
+                }
+                XRa = XRn;
+                respuesta.add(renglon);
+                if (Ea <= biseccion.getEa()) {
+                    break;
+                }
+            }
+        } else {
+           Biseccion renglon = new Biseccion();
+            //renglon.setIntervaloInvalido(true);
+            respuesta.add(renglon);
+        }
+
+        return respuesta;
+    }
+
+
+
+ @Override
+  
+public ArrayList<ReglaFalsa> AlgoritmoReglaFalsa(ReglaFalsa reglafalsa) {
+    ArrayList<ReglaFalsa> respuestaReglaFalsa = new ArrayList<>();
+    double XL, XU, XRa = 0, XRn, FXL, FXU, FXR, Ea = 100;
+    XL = reglafalsa.getXL();
+    XU = reglafalsa.getXU();
+    
+    FXL = Funciones.Ecuacion(reglafalsa.getFX(), XL);
+    FXU = Funciones.Ecuacion(reglafalsa.getFX(), XU);
+    
+    if (FXL * FXU < 0) {
+        for (int i = 1; i <= reglafalsa.getIteracionesMaximas(); i++) {
+            // Fórmula de Regla Falsa
+            XRn = XU - (FXU * (XL - XU)) / (FXL - FXU);
+            FXR = Funciones.Ecuacion(reglafalsa.getFX(), XRn);
+            
+            if (i != 1) {
+                Ea = Funciones.ErrorRelativo(XRn, XRa);
+            }
+            
+            ReglaFalsa renglon = new ReglaFalsa();
+            renglon.setIteracionesMaximas(i);
+            renglon.setXL(XL);
+            renglon.setXU(XU);
+            renglon.setXR(XRn);
+            renglon.setFXL(FXL);
+            renglon.setFXU(FXU);
+            renglon.setFXR(FXR);
+            renglon.setEa(Ea);
+            
+            // AGREGAR ESTA LÍNEA PARA CALCULAR EL PRODUCTO
+            renglon.setProductoFXL_FXR(FXL * FXR);
+            
+            if (FXL * FXR < 0) {
+                XU = XRn;
+                FXU = FXR;
+            } else {
+                XL = XRn;
+                FXL = FXR;
+            }
+            
+            XRa = XRn;
+            respuestaReglaFalsa.add(renglon);
+            
+            if (Ea <= reglafalsa.getEa()) {
+                break;
+            }
+        }
+    } else {
+        respuestaReglaFalsa.add(new ReglaFalsa());
+    }
+    return respuestaReglaFalsa;
+}
+}
